47e1048a819a6b199644cf69af485f8b27226baa
Salvador <csalvador58@gmail.com>
2022-08-09 22:17:06 -0700

    Subgraph yaml file built

--- src/web-3-rsvp.ts
+++ src/web-3-rsvp.ts
@@ -3,16 +3,16 @@ import {
   ConfirmedAttendee,
   DepositsPaidOut,
   NewEventCreated,
-  NewRSVP
+  NewRSVP,
 } from "../generated/Web3RSVP/Web3RSVP"
 import { Account, RSVP, Confirmation, Event } from "../generated/schema";
-import { integer } from "@protofire/subgraphs-toolkit";
+import { integer } from "@protofire/subgraph-toolkit";
 
 export function handleNewEventCreated(event: NewEventCreated): void {
   let newEvent = Event.load(event.params.eventId.toHex());
   if (newEvent == null) {
     newEvent = new Event(event.params.eventId.toHex());
-    newEvent.eventId = event.params.eventId;
+    newEvent.eventID = event.params.eventId;
     newEvent.eventOwner = event.params.creatorAddress;
     newEvent.eventTimestamp = event.params.eventTimestamp;
     newEvent.maxCapacity = event.params.maxCapacity;
@@ -65,9 +65,65 @@ export function handleNewEventCreated(event: NewEventCreated): void {
 }
 
 
-export function handleNewRSVP(event: NewRSVP): void {}
+function getOrCreateAccount(address: Address): Account {
+  let account = Account.load(address.toHex());
+  if (account == null) {
+    account = new Account(address.toHex());
+    account.totalRSVPs = integer.ZERO;
+    account.totalAttendedEvents = integer.ZERO;
+    account.save();
+  }
+  return account;
+}
 
-export function handleDepositsPaidOut(event: DepositsPaidOut): void {}
 
-export function handleConfirmedAttendee(event: ConfirmedAttendee): void {}
+export function handleNewRSVP(event: NewRSVP): void {
+  let id = event.params.eventId.toHex() + event.params.attendeeAddress.toHex();
+  let newRSVP = RSVP.load(id);
+  let account = getOrCreateAccount(event.params.attendeeAddress);
+  let thisEvent = Event.load(event.params.eventId.toHex());
+  if (newRSVP == null && thisEvent != null) {
+    newRSVP = new RSVP(id);
+    newRSVP.attendee = account.id;
+    newRSVP.event = thisEvent.id;
+    newRSVP.save();
+    thisEvent.totalRSVPs = integer.increment(thisEvent.totalRSVPs);
+    thisEvent.save();
+    account.totalRSVPs = integer. increment(account.totalRSVPs);
+    account.save();
+  }
+}
+
+
+export function handleConfirmedAttendee(event: ConfirmedAttendee): void {
+  let id = event.params.eventId.toHex() + event.params.attendeeAddress.toHex();
+  let newConfirmation = Confirmation.load(id);
+  let account = getOrCreateAccount(event.params.attendeeAddress);
+  let thisEvent = Event.load(event.params.eventId.toHex());
+  if (newConfirmation == null && thisEvent != null) {
+    newConfirmation = new Confirmation(id);
+    newConfirmation.attendee = account.id;
+    newConfirmation.event = thisEvent.id;
+    newConfirmation.save();
+    thisEvent.totalConfirmedAttendees = integer.increment(
+      thisEvent.totalConfirmedAttendees
+    );
+    thisEvent.save();
+    account.totalAttendedEvents = integer.increment(
+      account.totalAttendedEvents
+    );
+    account.save();
+  }
+}
+
+
+
+
+export function handleDepositsPaidOut(event: DepositsPaidOut): void {
+  let thisEvent = Event.load(event.params.eventId.toHex());
+  if (thisEvent) {
+    thisEvent.paidOut = true;
+    thisEvent.save();
+  }
+}
 
